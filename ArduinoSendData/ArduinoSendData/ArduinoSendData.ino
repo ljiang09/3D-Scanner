50c255dda6a1c1e90e6895e3d9dfbda08932573e
Lily Jiang <91999599+ljiang09@users.noreply.github.com>
2022-09-25 21:48:20 -0400

    Add commands for servo 2 movement

--- ArduinoSendData/ArduinoSendData/ArduinoSendData.ino
+++ ArduinoSendData/ArduinoSendData/ArduinoSendData.ino
@@ -1,4 +1,3 @@
-
 //      ******************************************************************
 //      *                                                                *
 //      *                                                                *
@@ -8,50 +7,66 @@
 //      ******************************************************************
 
 
-//
-// setup function to initialize hardware and software
-//
+// initialize hardware and software. Servos need to be in PWM pins
 
+// servo 1 controls the vertical movement, servo 2 controls the horizontal movement
 #include <Servo.h>
 Servo servo1;
+Servo servo2;
 
-int pos = 0;
 const int servo1Pin = 3;
+const int servo2Pin = 5;
 const int sensorPin = A0;
 
-int sensorValue = 0;        // value read from the pot
+int sensorValue = 0;  // value read in from the sensor
+int pos1 = 0;
+int pos2 = 0;
+
+// these increments determine how much each servo will move between collecting data
+const int servo1Increment = 4;
+const int servo2Increment = 1;
+
+// toggles each time the horizontal servo reaches the end of its pass, so the next pass will be in the opposite direction
+bool positiveRotation = true;
+
+const String stopKeyword = "STOP";  // we print this out to the serial port to signal the python code to stop fetching data
 
 void setup()
-{ 
-  //
-  // start the serial port
-  //
+{
   long baudRate = 9600;       // NOTE1: The baudRate for sending & receiving programs must match
   Serial.begin(baudRate);     // NOTE2: Set the baudRate to 115200 for faster communication
-  servo1.attach(3);
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
 }
 
-void loop() 
-{  
-    for (pos = 0; pos <= 20; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      // tell servo to go to position in variable 'pos'
-      servo1.write(pos);
-      sensorValue = analogRead(sensorPin);
-      Serial.println(sensorValue);
-      // read the analog in value:
-      delay(15);                       // waits 15 ms for the servo to reach the position
+void loop()
+{
+    for (pos1 = 0; pos1 <= 20; pos1 += servo1Increment) {   // control servo 1
+        
+        // in steps of 1 degree, tell servo1 to go to position in variable 'pos1'
+        servo1.write(pos1);
+
+        // goes from 0 degrees to 180 degrees
+        if (positiveRotation) {
+            for (pos2 = 0; pos2 <= 20; pos2 += servo2Increment) {
+                servo2.write(pos2);
+                sensorValue = analogRead(sensorPin);
+                Serial.println(sensorValue);
+                delay(15);
+            }
+        }
+
+        // goes from 180 degrees to 0 degrees
+        else {
+            for (pos2 = 20; pos2 >= 0; pos2 -= servo2Increment) {
+                servo2.write(pos2);
+                sensorValue = analogRead(sensorPin);
+                Serial.println(sensorValue);
+                delay(15);
+            }
+        }
+
+        delay(400);
     }
-    //
-    // delay after sending data so the serial connection is not over run
-    //
-    delay(400);
-    for (pos = 20; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      servo1.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15 ms for the servo to reach the position
-    }
-   
+    
 }
-
-
-
