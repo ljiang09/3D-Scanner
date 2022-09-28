Fixed formula for exponential

python_receive_data.py
@@ -3,6 +3,8 @@ TODO:
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import csv
 
 import sensor_calibration as calibrate
 
@@ -19,7 +21,6 @@ def fetch_data(serial_port, get_data, params):
 
     try:
       data_line = data_line.decode("utf-8")
-      print(data_line)
       # End fetching if "STOP" is read in from Arduino code
       if data_line == "STOP\r\n":
         get_data = False
@@ -30,10 +31,9 @@ def fetch_data(serial_port, get_data, params):
 
         if len(data_split) == 3:
           try:
-            data_float = [float(x) for x in data_split] 
             sensor_volt.append(float(data_split[0]) * 5 / 1023)    
-            position_degrees[1].append(float(data_split[1]))
-            position_degrees[0].append(float(data_split[2]))
+            position_degrees[0].append(float(data_split[1]))
+            position_degrees[1].append(float(data_split[2]))
           except:
             print("Got empty result")
 
@@ -56,7 +56,8 @@ def angle_to_coordinates(sensor_volt, position_degrees, params):
   tilt_degrees = np.array(position_degrees[1])
 
   # Data to distances
-  radii = calibrate.exponential(sensor_volt, *params)
+  radii = calibrate.inv_exponential(sensor_volt, *params)
+  print(sensor_volt, radii)
 
   # Center the angles so that 90 degrees is 0 (At '90 degrees', new 0, distance = radius) for x + y
   pan_degrees -= 30 # phi
@@ -64,25 +65,36 @@ def angle_to_coordinates(sensor_volt, position_degrees, params):
 
   positions = [[], [], []]
 
+  f = open("n_data.csv", 'w')
+  writer = csv.writer(f)
+  writer.writerow(["x", "y", "z", "radii"])
+
   for index in range(len(pan_degrees)):
 
     # Convert degrees to radians
     pan_radian = np.radians(pan_degrees[index])
     tilt_radian = np.radians(tilt_degrees[index])
 
+    x = radii[index] * np.cos(tilt_radian) * np.sin(pan_radian)
+    y = radii[index] * np.sin(tilt_radian)
+    z = radii[index] * np.cos(tilt_radian) * np.cos(pan_radian)
+
     # Convert spherical to cartesian
     # y = radii[index] * np.sin(tilt_radian)
     # x = radii[index] * np.cos(tilt_radian) * np.cos(pan_radian)
     # z = radii[index] * np.cos(tilt_radian) * np.sin(pan_radian)
 
-    y = radii[index] * np.cos(tilt_radian)
-    z = radii[index] * np.cos(tilt_radian) * np.cos(pan_radian)
-    x = radii[index] * np.cos(tilt_radian) * np.sin(pan_radian)
+    # y = radii[index] * np.sin(tilt_radian)
+    # x = radii[index] * np.sin(pan_radian)
+    # z = radii[index] * np.cos(tilt_radian) * np.cos(pan_radian)
 
     positions[0].append(x)
     positions[1].append(y)
     positions[2].append(z)
-    print(positions)
+
+    writer.writerow([positions[0], positions[1], positions[2], radii[index]])
+
+  f.close()
 
   return np.array(positions), radii
 
@@ -92,8 +104,8 @@ def plot_heatmap(positions, radii):
   """
   fig = plt.figure()
   ax = plt.axes(projection ='3d')
-  ax.plot(positions[0], positions[1], positions[2], 'green')
-  ax.scatter(positions[0], positions[1], radii)
+  # ax.scatter(positions[0], positions[1], positions[2], 'green')
+  ax.scatter(positions[0], positions[1], positions[2])
   ax.set_xlabel("X")
   ax.set_ylabel("Y")
   plt.show()

sensor_calibration.py
@@ -5,20 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-# Readings taken at 4 inch intervals starting at 8 inches
-DISTANCES = np.array(range(8, 45, 4))
-READINGS = np.array([490, 385, 300, 240, 210, 185, 170, 157, 153, 145])
+# Readings taken at 4 cm intervals starting at 20 cm
+DISTANCES = np.array([20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 90, 110, 120, 130, 140])
+READINGS = np.array([500, 470, 410, 395, 345, 315, 286, 255, 240, 210, 195, 180, 170, 160, 120, 95, 85, 80, 75]) #300, 240, 210, 185, 170, 157, 153, 145])
 
 # Readings taken at distances (cm) specified in 
-TEST_DATA = np.array([[30, 40, 27, 66.04, 53.34, 73.66], [390, 307, 415, 196, 240, 193]])
+TEST_DATA = np.array([[30, 38, 43, 53, 66, 73], [400, 330, 298, 230, 180, 160]])
 
 def exponential(x, a, b, c):
     """
+    Distance to Voltage
     TODO:
     """
     return (a * np.exp((b * x))) + c
 
-def generate_calibration_plot(distances, readings):
+def inv_exponential(v, a, b, c):
+    """
+    Voltage to Distance
+    TODO:
+    """
+    return (np.log((v - c)/a)/b)
+
+def generate_calibration_plot(distances_cm, readings):
     """
     Based on data generated from an IR sensor located at different distances from a wall, a
     calibration plot is created
@@ -31,7 +39,7 @@ def generate_calibration_plot(distances, readings):
         params: TODO: WRITE THIS IN
     """
     # Convert to correct units
-    distances_cm = 2.54 * distances
+    # distances_cm = 2.54 * distances
     readings_volt = readings * 5 / 1023
 
     # Plot reading against 1/distance
@@ -42,7 +50,7 @@ def generate_calibration_plot(distances, readings):
 
     # Fit line
     param, cov = curve_fit(f=exponential, xdata=distances_cm, ydata=readings_volt,
-        bounds=(-np.inf, np.inf), p0=[750.0, -0.1, 100.0])
+        bounds=(-np.inf, np.inf), p0=[850.0, -0.01, -50.0])
     print(f"Curve Fit Equation: {param[0]} * e ^ {param[1]}x + {param[2]}")
 
     # Plot fitted line
@@ -72,7 +80,7 @@ def run_calibration():
     TODO:
     """
     params = generate_calibration_plot(DISTANCES, READINGS)
-    test_calibration_curve(DISTANCES * 2.54, params, TEST_DATA)
+    test_calibration_curve(DISTANCES, params, TEST_DATA)
     return params
 
 