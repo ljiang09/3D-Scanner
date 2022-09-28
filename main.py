925f9e1512b243eefe00c796381f4bb807cc9239
Rucha Dave <ruchadave16@gmail.com>
2022-09-27 23:03:56 -0400

    Add somewhat good graph

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import sensor_calibration as calibrate
 import python_receive_data as prd
 
 # Link to port
-arduino_com_port = "/dev/ttyACM0"
+arduino_com_port = "/dev/ttyACM1"
 baud_rate = 9600
 serial_port = serial.Serial(arduino_com_port, baud_rate, timeout=10)
 
 # Calibrate, plot, and fetch parameters of equation
 params = calibrate.run_calibration()
 
+# distance_this = calibrate.inv_exponential(280 * 5 / 1023, *params)
+# print(prd.angle_to_coordinates([280 * 5 / 1023], [[0], [0]], params))
 # Fetch data until STOP command given
 sensor_volt, positions, radii = prd.fetch_data(serial_port, get_data=True, params=params)
 
--- python_receive_data.py
+++ python_receive_data.py
@@ -3,6 +3,7 @@ TODO:
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import math
 import csv
 
@@ -14,6 +15,10 @@ def fetch_data(serial_port, get_data, params):
   """
   sensor_volt = []
   position_degrees = [[], []]
+
+  f = open("recording_data1.csv", "w")
+  writer = csv.writer(f)
+  writer.writerow(["value", "pan_degree", "tilt_degree"])
 
   # Fetch data
   while get_data:
@@ -32,17 +37,21 @@ def fetch_data(serial_port, get_data, params):
         if len(data_split) == 3:
           try:
             sensor_volt.append(float(data_split[0]) * 5 / 1023)    
-            position_degrees[0].append(float(data_split[1]))
-            position_degrees[1].append(float(data_split[2]))
+            position_degrees[0].append(float(data_split[1])) # Pan
+            position_degrees[1].append(float(data_split[2])) # Tilt
           except:
             print("Got empty result")
 
         print(data_split)
+        writer.writerow([float(data_split[0]), float(data_split[1]), float(data_split[2])])
 
     except:
       print("Couldn't decode the line")
 
+  f.close()
+
   sensor_volt = np.array(sensor_volt)
+
   positions, radii = angle_to_coordinates(sensor_volt, position_degrees, params)
   return (sensor_volt, positions, radii)
 
@@ -57,27 +66,25 @@ def angle_to_coordinates(sensor_volt, position_degrees, params):
 
   # Data to distances
   radii = calibrate.inv_exponential(sensor_volt, *params)
-  print(sensor_volt, radii)
 
-  # Center the angles so that 90 degrees is 0 (At '90 degrees', new 0, distance = radius) for x + y
-  pan_degrees -= 30 # phi
-  tilt_degrees -= 10 # theta
+  # # Center the angles so that 90 degrees is 0 (At '90 degrees', new 0, distance = radius) for x + y
+  # pan_degrees -= 30 # phi
+  # tilt_degrees -= 10 # theta
 
   positions = [[], [], []]
 
-  f = open("n_data.csv", 'w')
+  f = open("n_data1.csv", 'w')
   writer = csv.writer(f)
   writer.writerow(["x", "y", "z", "radii"])
 
   for index in range(len(pan_degrees)):
-
     # Convert degrees to radians
     pan_radian = np.radians(pan_degrees[index])
     tilt_radian = np.radians(tilt_degrees[index])
 
-    x = radii[index] * np.cos(tilt_radian) * np.sin(pan_radian)
-    y = radii[index] * np.sin(tilt_radian)
-    z = radii[index] * np.cos(tilt_radian) * np.cos(pan_radian)
+    x = radii[index] * np.sin((np.pi / 2) - tilt_radian) * np.cos((np.pi / 2) - pan_radian)
+    y = radii[index] * np.sin((np.pi / 2) - tilt_radian) * np.sin((np.pi / 2) - pan_radian)
+    z = radii[index] * np.cos((np.pi / 2) - tilt_radian)
 
     # Convert spherical to cartesian
     # y = radii[index] * np.sin(tilt_radian)
@@ -92,11 +99,21 @@ def angle_to_coordinates(sensor_volt, position_degrees, params):
     positions[1].append(y)
     positions[2].append(z)
 
-    writer.writerow([positions[0], positions[1], positions[2], radii[index]])
+    writer.writerow([x, y, z, radii[index]])
 
   f.close()
-
+  positions = filter_data(positions)
   return np.array(positions), radii
+
+def filter_data(positions):
+  """
+  TODO:
+  """
+  x = positions[0]
+  y = positions[1]
+  z = positions[2]
+
+  
 
 def plot_heatmap(positions, radii):
   """
@@ -105,9 +122,10 @@ def plot_heatmap(positions, radii):
   fig = plt.figure()
   ax = plt.axes(projection ='3d')
   # ax.scatter(positions[0], positions[1], positions[2], 'green')
-  ax.scatter(positions[0], positions[1], positions[2])
+  ax.scatter3D(positions[0], positions[1], positions[2])
   ax.set_xlabel("X")
   ax.set_ylabel("Y")
+  ax.set_zlabel("Z")
   plt.show()
   plt.title("Heatmap of Distances")
   
