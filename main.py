939741ace753a6fb8424d490cd6c9fd57c236195
Lily Jiang <91999599+ljiang09@users.noreply.github.com>
2022-09-27 23:33:08 -0400

    add/edit documentation

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 """
-TODO:
+Driver file for calibration curves and sensor visualization.
+
+Generates a sensor calibration curve, validates the curve.
+Listens for data from the Arduino scanner, generates a 3D visualization of the scanned object
 """
 import serial
 import sensor_calibration as calibrate
 import python_receive_data as prd
 
-# Link to port
-arduino_com_port = "/dev/ttyACM1"
-baud_rate = 9600
+# Link to port for communication with Arduino
+arduino_com_port = "/dev/ttyACM0"
+baud_rate = 9600   # this value must be the exact same as in the Arduino code
 serial_port = serial.Serial(arduino_com_port, baud_rate, timeout=10)
 
-# Calibrate, plot, and fetch parameters of equation
+# Calibrate, plot, and fetch parameters of sensor reading vs. distance equation
 params = calibrate.run_calibration()
+
 
 # distance_this = calibrate.inv_exponential(280 * 5 / 1023, *params)
 # print(prd.angle_to_coordinates([280 * 5 / 1023], [[0], [0]], params))
+
+
 # Fetch data until STOP command given
 sensor_volt, positions, radii = prd.fetch_data(serial_port, get_data=True, params=params)
 
-# Generate image of object
+# Generate image of scanned object
 prd.plot_heatmap(positions, radii)
